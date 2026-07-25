af627bcc1b5eb7d71dfce88c10a02e860e42592c
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-24 17:26:19 -0700

    Update the.py

--- the.py
+++ the.py
@@ -20,7 +20,7 @@ if sign=='/':
 st.title("أهلا بك في لعبتي ")
 st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
-if st.button("تاكيد الاجابه ")
+if st.button("تاكيد الاجابه "):
  if number==sc:
  print("you are winner ")
 session_state.num=+1
