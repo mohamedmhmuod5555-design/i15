7813bcb96db346a2159c7c60b0dc9e61029cfed1
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-24 17:24:25 -0700

    Update the.py

--- the.py
+++ the.py
@@ -24,8 +24,8 @@ if st.button("تاكيد الاجابه "):
  if number==sc:
   print("you are winner ")
 session_state.num=+1
-else:
- print("your answeris not true ") 
+ else:
+  print("your answeris not true ") 
 if st.button(" السؤال التالي "):
  del session_state.num1 
  del session_state.num2
